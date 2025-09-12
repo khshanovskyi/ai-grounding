a41c0ade0c0cdcb67dd7ff120e12250a2d27ebf6
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-09-12 08:32:19 +0300

    Initial commit

--- task/t3/in_out_grounding.py
+++ task/t3/in_out_grounding.py
@@ -14,7 +14,7 @@ from task.user_client import UserClient
 #TODO: Info about app:
 # HOBBIES SEARCHING WIZARD
 # Searches users by hobbies and provides their full info in JSON format:
-#   Input: `In need to gather people that love to go to mountains`
+#   Input: `I need to gather people that love to go to mountains`
 #   Output:
 #     ```json
 #       "rock climbing": [{full user info JSON},...],
